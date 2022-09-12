bf233e9bc95d2835416b82b282e878de495a3082
Kofi003 <79612940+Kofi003@users.noreply.github.com>
2022-09-12 00:05:16 +0000

    0x0F-python-object_relational_mapping/Submission

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
-"""lists all states from the database 'hbtn_0e_0_usa'"""
-
+"""
+Module list state
+"""
+import sys
 import MySQLdb
-from sys import argv
+
+
+def main():
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cur = conn.cursor()
+    query = "SELECT id,name FROM states ORDER by id ASC"
+    cur.execute(query)
+    row = cur.fetchall()
+    for r in row:
+        print(r)
+    cur.close()
+    conn.close()
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
-    for data in cursor.fetchall():
-        print(data)
-    cursor.close()
-    db.close()
+    main()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
 """
-Defines a City model.
-Inherits from SQLAlchemy Base and links to the MySQL table cities.
+Write a python file that contains the class
+definition of a State
 """
-
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
 
 
-class City(Base):
-    """Represents a city for a MySQL database.
-
-    Attributes:
-        id (str): The city's id.
-        name (sqlalchemy.Integer): The city's name.
-        state_id (sqlalchemy.String): The city's state id.
+class State(Base):
     """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+        State class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
